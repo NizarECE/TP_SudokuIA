909c4a7566c0e2535270ed8f232128d1c3ef081e
MaevaTF <maeva.tchetchoua-feze@edu.ece.fr>
2020-01-21 11:15:54 +0100

    * ISudokuSolver.cs: ajout du code Z3 en brut à adapter
    * Z3.cs:
    * TP_SudokuIA.sln:
    * Program.cs:
    * SolverZ3.csproj:
    * Sudoku_Easy50.txt:

---
+++ SolverZ3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NoyauTP;
+
+namespace SolverZ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var s = new Sudoku();
+     
+            s.newEasySudoku(0);
+            var solver = new Z3();
+            var solution = solver.Solve(s);
+            solution.showSudoku();
+            Console.Read();
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
