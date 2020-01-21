fe0fbabc7f40fbcb4e6a7bd092d595697091d75a
Adrien863 <adrienpatry3@gmail.com>
2020-01-21 11:05:27 +0100

    Ajout SudokuSolver_ORTools

---
+++ SudukoSolver_ORTools/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NoyauTP;
+
+namespace SudukoSolver_ORTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Sudoku();
+            s.showSudoku();
+            Console.Read();
+        }
+    }
+}
