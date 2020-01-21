cb0d65d3564942cb8199cbff3a4d462555cd6dfb
SylvainBnstn <sylvain.benistand@edu.ece.fr>
2020-01-21 10:27:55 +0100

    Avancé

---
+++ Norvig_method/Norvig.cs
@@ -0,0 +1,15 @@
+﻿using NoyauTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Norvig_method
+{
+    public class Norvig : ISudokuSolver
+    {
+        public Sudoku Solve(Sudoku s)
+        {
+            return s;
+        }
+    }
+}
---
+++ Norvig_method/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NoyauTP;
+
+namespace Norvig_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Sudoku();
+            s.newEasySudoku(0);
+            var solveur = new Norvig();
+            var solution = solveur.Solve(s);
+            solution.showSudoku();
+        }
+    }
+}
