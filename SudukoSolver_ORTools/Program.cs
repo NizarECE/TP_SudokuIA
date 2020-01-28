39bd2a038f4d9afaf45b6dfe1654d881c0248e74
Adrien863 <adrienpatry3@gmail.com>
2020-01-28 09:29:00 +0100

    Ajout des commentaires

--- SudukoSolver_ORTools/Program.cs
+++ SudukoSolver_ORTools/Program.cs
@@ -8,12 +8,24 @@ namespace SudukoSolver_ORTools
     {
         static void Main(string[] args)
         {
+            // Création d'un sudoku aléatoire de niveau facile
             Sudoku s = new Sudoku();
             s.newEasySudoku(-1);
+
+            // Affichage du sudoku initial généré
             s.showInitialSudoku();
+
+            // Passage du type Sudoku à String pour l'exécution du script python
             String sudoku_string = s.sudokuToString(s.getInitialSudoku());
+
+            // Solver
             String sudoku_resolu_string = Solver_ORTools.Exe(sudoku_string);
+
+            // Retour au type Sudoku pour l'affichage
             int[][] sudoku_resolu = s.stringToSudoku(sudoku_resolu_string);
+
+            // Affichage du résultat
+            Console.WriteLine("RESULTS:");
             s.show(sudoku_resolu);
             Console.Read();
         }
--- SudukoSolver_ORTools/Solver_ORTools.cs
+++ SudukoSolver_ORTools/Solver_ORTools.cs
@@ -7,31 +7,36 @@ namespace SudokuSolver_ORTools
     {
         public static String Exe(string sudoku)
         {
-
+            // Création du process d'exécution du shell python
             var psi = new ProcessStartInfo();
-            psi.FileName = @"C:\Users\Adrien\Anaconda3\python.exe";
 
+            // Chemins d'accès du shell et du script exécuté                 !!! Bien vérifier le chemin entré !!!
+            psi.FileName = @"C:\Users\Adrien\Anaconda3\python.exe";
             var script = @"C:\Users\Adrien\Desktop\ECE\ING4\Intelligence artificielle\sudoku_ortools.py";
             
-
+            // Ajout des arguments à passer au shell et des paramètres d'exécution
             psi.ArgumentList.Add(script);
             psi.ArgumentList.Add(sudoku);
-
             psi.UseShellExecute = false;
             psi.CreateNoWindow = true;
             psi.RedirectStandardOutput = true;
             psi.RedirectStandardError = true;
 
+            // Création des variables de sorties d'exécution
             var errors = "";
             var results = "";
 
+            // Exécution du script
             using(var process = Process.Start(psi))
             {
+
+                // Récupération des sorties d'exécution
                 errors = process.StandardError.ReadToEnd();
                 results = process.StandardOutput.ReadToEnd();
                
             }
 
+            // Affichage des erreurs
             Console.WriteLine("ERRORS:");
             if (errors == "")
             {
@@ -42,7 +47,7 @@ namespace SudokuSolver_ORTools
                 Console.WriteLine(errors);
             }
 
-            Console.WriteLine("RESULTS:");
+            // Renvoie le resultat
             results = results.Replace(", ","");
             results = results.Replace("][","");
             results = results.Replace("[", "");
